746ca0bad33e71b129be25aff7c9b5f9bbbc2c73
Jean F. dos Santos <jeanddg@hotmail.com>
2024-10-20 19:53:00 -0300

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,14 +7,14 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import { errorHandler } from './validation/errorHandler.js';
-import { userRoutes } from './user/userRouter.js';
+import { authRoutes } from './auth/authRoutes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
-app.use('/auth', userRoutes);
+app.use('/auth', authRoutes);
 app.use(errorHandler);
 
 export { app };
