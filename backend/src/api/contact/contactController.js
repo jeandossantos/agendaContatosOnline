f89d0084da06a57697f4936355e3b6e14097c64a
Jean F. dos Santos <jeanddg@hotmail.com>
2024-10-21 13:54:19 -0300

    feat: add update in contactController

--- backend/src/api/contact/contactController.js
+++ backend/src/api/contact/contactController.js
@@ -1,4 +1,5 @@
-import { HttpStatus } from 'http-exception-library';
+import { BadRequestException, HttpStatus } from 'http-exception-library';
+import { isEmailOrError } from '../../validation/validators.js';
 
 export class ContactController {
   #contactRepository;
@@ -9,10 +10,12 @@ export class ContactController {
 
   async handleCreate(req, res) {
     const { user_id } = req;
-    const { phone_number, phone_number_2, email, email_2, address } = req.body;
+    const { name, phone_number, phone_number_2, email, email_2, address } =
+      req.body;
 
     await this.#contactRepository.create({
       user_id,
+      name,
       phone_number,
       phone_number_2,
       email,
@@ -37,5 +40,48 @@ export class ContactController {
     const contact = await this.#contactRepository.show(parseInt(id));
 
     return res.json(contact); // Retorna o contato encontrado
+  }
+
+  async handleFind(req, res) {
+    const { user_id } = req;
+    const { search, page, limit } = req.query;
+
+    const contacts = await this.#contactRepository.listUserContacts({
+      user_id,
+      search,
+      limit,
+      page,
+    });
+
+    return res.json(contacts);
+  }
+
+  async handleUpdate(req, res) {
+    const { id } = req.params;
+    const { name, phone_number, phone_number_2, email, email_2, address } =
+      req.body;
+
+    if (email) {
+      isEmailOrError(email, 'Informe e-mail válido.', BadRequestException);
+    }
+
+    if (email_2) {
+      isEmailOrError(
+        email_2,
+        'Informe segundo e-mail válido.',
+        BadRequestException
+      );
+    }
+
+    const updatedContact = await this.#contactRepository.update(id, {
+      name,
+      phone_number,
+      phone_number_2,
+      email,
+      email_2,
+      address,
+    });
+
+    return res.json(updatedContact);
   }
 }
